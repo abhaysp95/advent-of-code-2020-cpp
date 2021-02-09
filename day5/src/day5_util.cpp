5f0530e25d10e9a30d0062c96baa371eb5b3702e
coolabhays <abhaysp9955@gmail.com>
2021-02-09 21:41:13 +0530

    part2 of day5 completed

--- day5/src/day5_util.cpp
+++ day5/src/day5_util.cpp
@@ -2,6 +2,8 @@
 
 #include "../inc/day5_util.hpp"
 
+#include <algorithm>
+
 size_t get_place(std::string::const_iterator& begin, const std::string::const_iterator& end,
 		size_t beg_place, size_t end_place, char first, char second) {
 	size_t mid_place = (beg_place + end_place) / 2;
@@ -18,4 +20,59 @@ size_t get_place(std::string::const_iterator& begin, const std::string::const_it
 		}
 	}
 	return mid_place;
+}
+
+/** question says that my seat isn't in very first and very last, so I think,
+  * we can skip first 8 seats and last eigth seats for checking
+  */
+size_t get_my_seat_id(std::vector<size_t>& seat_ids) {
+	std::sort(seat_ids.begin(), seat_ids.end(), [=](size_t first, size_t second) { return first < second; });
+	std::vector<size_t>::const_iterator begin_from{seat_ids.begin() + 8}, end_is{seat_ids.end() - 8};
+	std::vector<size_t>::const_iterator prev_seat = begin_from - 1;
+	while (begin_from != end_is) {
+		/**
+		  * if current_seat isn't just the increment of prev_seat then
+		  * if value of prev_seat + 1 is equals to value of current seat and
+		  * if value of current_seat + 1 is equals to value of next seat, then
+		  * current seat is my seat
+		  */
+		if ((*prev_seat + 1) != *begin_from) {
+			// check if next seat from my seat for my seat exist or not
+			if ((*prev_seat + 2) == *begin_from) {
+				return *begin_from - 1;
+			}
+		}
+		prev_seat = begin_from;
+		begin_from++;
+	}
+	return 0;
+}
+
+/** question says that my seat isn't in very first and very last, so I think,
+  * we can skip first 8 seats and last eigth seats for checking
+  */
+std::string get_my_seat_path(const std::map<std::string, size_t>& seats_info) {
+	// sort the seats
+	//std::sort(seats_info.begin(), seats_info.end(),
+			//[=](const std::pair<const std::string, size_t>& first, const std::pair<const std::string, size_t>& second) {
+					//return first.second < second.second;
+				//});
+	std::map<std::string, size_t>::const_iterator
+		second_from_start{std::next(seats_info.begin(), 8)},
+		second_from_end{std::prev(seats_info.end(), 8)};
+	// set counter from second seat
+	size_t seat_id_counter = second_from_start->second;
+	while (second_from_start != second_from_end) {
+		if (second_from_start->second != seat_id_counter) {
+			// check if +1 and -1 seats exist or not
+			if ((std::prev(second_from_start, 1)->second == seat_id_counter - 1) &&
+					(std::next(second_from_start, 1)->second == seat_id_counter + 1)) {
+				// this is my seat
+				return second_from_start->first;
+			}
+		}
+		second_from_start = std::next(second_from_start, 1);
+		seat_id_counter++;
+	}
+	return nullptr;
 }
--- day5/src/main.cpp
+++ day5/src/main.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <map>
 
 #include "../inc/day5_util.hpp"
 
@@ -29,7 +30,49 @@ void part1() {
 	input_file.close();
 }
 
+void part2() {
+	std::ifstream input_file;
+	input_file.open("./input_main.txt");
+	std::vector<size_t> seat_ids;
+	std::string pos_str;
+	while (input_file >> pos_str) {
+		std::string pos_row(pos_str.begin(), (pos_str.begin() + 7));
+		std::string pos_col(pos_str.begin() + 7, pos_str.end());
+		std::string::const_iterator pos_row_begin{pos_row.begin()}, pos_row_end{pos_row.end()};
+		std::string::const_iterator pos_col_begin{pos_col.begin()}, pos_col_end{pos_col.end()};
+		size_t get_row_pos = get_place(pos_row_begin, pos_row_end, 0, 127, 'F', 'B');
+		size_t get_col_pos = get_place(pos_col_begin, pos_col_end, 0, 7, 'L', 'R');
+		size_t seat_id = get_row_pos * 8 + get_col_pos;
+		seat_ids.push_back(seat_id);
+	}
+	size_t my_seat_number = get_my_seat_id(seat_ids);
+	std::cout << "My seat is: " << my_seat_number << std::endl;
+	input_file.close();
+}
+
+void more() {
+	std::ifstream input_file;
+	input_file.open("./input_demo.txt");
+	std::map<std::string, size_t> seats_info;
+	std::string pos_str;
+	while (input_file >> pos_str) {
+		std::string pos_row(pos_str.begin(), (pos_str.begin() + 7));
+		std::string pos_col(pos_str.begin() + 7, pos_str.end());
+		std::string::const_iterator pos_row_begin{pos_row.begin()}, pos_row_end{pos_row.end()};
+		std::string::const_iterator pos_col_begin{pos_col.begin()}, pos_col_end{pos_col.end()};
+		size_t get_row_pos = get_place(pos_row_begin, pos_row_end, 0, 127, 'F', 'B');
+		size_t get_col_pos = get_place(pos_col_begin, pos_col_end, 0, 7, 'L', 'R');
+		size_t seat_id = get_row_pos * 8 + get_col_pos;
+		seats_info.insert(std::pair<std::string, size_t>(pos_str, seat_id));
+	}
+	std::string my_seat_number = get_my_seat_path(seats_info);
+	if (!my_seat_number.empty()) {
+		std::cout << "My seat is: " << my_seat_number << std::endl;
+	}
+	input_file.close();
+}
+
 int main(int argc, char **argv) {
-	part1();
+	part2();
 	return 0;
 }
