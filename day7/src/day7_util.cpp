bdeaf6856d139e06a951da8036dc289ed951b9a9
coolabhays <abhaysp9955@gmail.com>
2021-02-12 08:02:06 +0530

    part2 of day7 completed

--- day7/src/day7_util.cpp
+++ day7/src/day7_util.cpp
@@ -86,7 +86,8 @@ std::string get_secondary_color_name(const std::string& bag_info) {
 }
 
 size_t get_bag_contain_count(const std::vector<std::string>& all_bags_info, const std::string color_name) {
-	static size_t bag_capacity{};
+	static size_t bag_capacity{};  // prev_bag_capacity was here
+	size_t temp_count{};
 	std::map<std::string, size_t> color_map;
 	std::vector<std::string>::const_iterator bags_info_cbegin{all_bags_info.cbegin()}, bags_info_cend{all_bags_info.cend()};
 	bool found_main_color = false;
@@ -102,16 +103,29 @@ size_t get_bag_contain_count(const std::vector<std::string>& all_bags_info, cons
 					map_elements_cend{color_map.end()};
 				do {
 					size_t count{map_elements_cbegin->second};
-					bag_capacity += count * get_bag_contain_count(all_bags_info, map_elements_cbegin->first);
+					/** tried to not use this extra loop(you can see the commented block at the very end) */
+					while (count != 0) {
+						size_t return_count = get_bag_contain_count(all_bags_info, map_elements_cbegin->first);
+						bag_capacity += return_count;
+						count--;
+					}
+					/** extra loop ends here */
 					map_elements_cbegin++;
 				} while (map_elements_cbegin != map_elements_cend);
 			}
-			bag_capacity += 1;  // check it later
+			temp_count += 1;
+			if (!color_map.empty()) {
+				//bag_capacity += 1;
+			}
 		}
 		if (found_main_color) { break; }
 		bags_info_cbegin++;
 	} while (bags_info_cbegin != bags_info_cend);
-	return bag_capacity;
+	// return the main count for my bag
+	if (color_name == "shiny gold") {
+		return bag_capacity;
+	}
+	return temp_count;
 }
 
 std::map<std::string, size_t> get_bag_details(const std::string& bag_info) {
@@ -123,7 +137,7 @@ std::map<std::string, size_t> get_bag_details(const std::string& bag_info) {
 	std::string::const_iterator first{bag_info.cbegin()}, second{}, bag_info_cend{bag_info.cend()};
 	do {
 		first = std::find_if(first, bag_info_cend, [=](const char& c){ return !isspace(c); });
-		second = std::find_if(first, bag_info_cend, [=](const char& c){ return !isspace(c); });
+		second = std::find_if(first, bag_info_cend, [=](const char& c){ return isspace(c); });
 		if (first != bag_info_cend) {
 			std::string temp_str{first, second};
 			if (number_found) {
@@ -140,10 +154,11 @@ std::map<std::string, size_t> get_bag_details(const std::string& bag_info) {
 				else {
 					color_name += temp_str + " ";
 				}
+				first = second;
 				continue;
 			}
 			try {
-				if (isdigit(stoi(temp_str))) {
+				if (stoi(temp_str)) {
 					number_found = true;
 					color_counts.push_back(stoi(temp_str));
 				}
@@ -162,3 +177,27 @@ std::map<std::string, size_t> get_bag_details(const std::string& bag_info) {
 	}
 	return map_for_color_count;
 }
+
+
+
+
+	/*do {
+		size_t count{map_elements_cbegin->second};
+		size_t return_count = get_bag_contain_count(all_bags_info, map_elements_cbegin->first);
+		//prev_bag_capacity = bag_capacity;
+		prev_bag_capacity += (return_count * count);
+		map_elements_cbegin++;
+	} while (map_elements_cbegin != map_elements_cend);
+	//bag_capacity += 1;
+}
+if (!color_map.empty()) {
+	// logic not correct
+	bag_capacity += prev_bag_capacity;
+	//temp_count += bag_capacity - prev_bag_capacity;  // answer 31, now
+	temp_count = prev_bag_capacity;
+	prev_bag_capacity = 0;
+}
+else {
+	temp_count += 1;  // do something like new bag_capacity - prev bag_capacity
+}
+//bag_capacity += 1;  // check it later*/
